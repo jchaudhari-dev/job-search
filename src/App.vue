0ac869a03b825e982446f029a8e3fe03699d2c63
jaykumarc <jaykumarchaudhari10@gmail.com>
2026-09-13 13:30:36 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <h1>Hello, I'm in App.vue</h1>
+
+    <HelloWorld msg="You did it!!" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./component/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: purple;
+}
+</style>
---
+++ src/component/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    msg:{
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div class="greetings">
+        <h1 class="green">{{msg}}</h1>
+        <h3>
+            Congraulations, you are now working with
+            <a href="https://vite.js.dev/" target="_blank"></a>
+        </h3>
+    </div>
+</template>
